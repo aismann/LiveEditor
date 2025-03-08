df84e4a64b1237f2778b66a6998849cb427d03bf
aismann <icesoft@freenet.de>
2025-03-08 11:23:43 +0100

    Update LiveEditor.h

--- extension/LiveEditor/include/LiveEditor.h
+++ extension/LiveEditor/include/LiveEditor.h
@@ -1,3 +1,13 @@
 #pragma once
 
-#include "LiveEditor/LiveEditor.h"
+NS_AX_EXT_BEGIN
+ 
+class LiveEditor
+{
+public:
+    static LiveEditor* getInstance();
+    void startPoint(ax::Scene*);
+    void close();
+};
+
+NS_AX_EXT_END
